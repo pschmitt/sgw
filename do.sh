c6cd9d01795b51baa591147433c0fa98571a15d8
Philipp Schmitt <philipp@schmitt.co>
2015-11-13 14:10:51 +0100

    Set SSH user in config file

--- do.sh
+++ do.sh
@@ -7,6 +7,12 @@ VENV_PATH=${WORKON_HOME:-$HOME/.local/share/virtualenvs}
 source $VENV_PATH/$VENV_NAME/bin/activate
 
 # Get token and droplet from config file
-source sgw.conf
+if [[ -r sgw.conf ]]
+then
+    source sgw.conf
+else
+    echo "Config file does not exist or is not readable" >&2
+    exit 3
+fi
 
 python do.py -t "$TOKEN" "$@" "$DROPLET"
